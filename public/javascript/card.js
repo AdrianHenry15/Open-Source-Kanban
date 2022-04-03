cea7f5f3caa6b8376d789bbe5cb5c95a36d844b0
Remnant <georgesimon4801@gmail.com>
2022-04-03 05:29:04 -0400

    structural css

--- public/javascript/card.js
+++ public/javascript/card.js
@@ -1,85 +1,81 @@
-const createTaskEl = function (taskDataObj) {
-    let listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.setAttribute("data-task-id", taskIdCounter);
+// const tasksToDoEl = document.querySelector("#tasks-to-do");
+// const tasksInProgressEl = document.querySelector("#tasks-in-progress");
+// const tasksCompletedEl = document.querySelector("#tasks-completed");
 
-    let taskInfoEl = document.createElement("div");
-    taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML =
-        "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-    listItemEl.appendChild(taskInfoEl);
 
-    let taskActionsEl = createTaskActions(taskIdCounter);
-    listItemEl.appendChild(taskActionsEl);
+// const createTaskEl = function (taskDataObj) {
 
-    // is not a select drop down anymore 
-    switch (taskDataObj.status) {
-        case "new ideas":
-            taskActionsEl.querySelector("select[name='status-change']").selectedIndex = 0;
-            tasksToDoEl.append(listItemEl);
-            break;
-        case "lets collaborate":
-            taskActionsEl.querySelector("select[name='status-change']").selectedIndex = 1;
-            tasksInProgressEl.append(listItemEl);
-            break;
-        case "old ideas":
-            taskActionsEl.querySelector("select[name='status-change']").selectedIndex = 2;
-            tasksCompletedEl.append(listItemEl);
-            break;
-        default:
-            console.log("Something went wrong!");
-    }
+//     let taskActionsEl = createTaskActions(taskIdCounter);
+//     listItemEl.appendChild(taskActionsEl);
 
-    // save task as an object with name, type, status, and id properties then push it into tasks array
-    taskDataObj.id = taskIdCounter;
+//     // is not a select drop down anymore 
+//     switch (taskDataObj.status) {
+//         case "new ideas":
+//             taskActionsEl.querySelector("select[name='status-change']").selectedIndex = 0;
+//             tasksToDoEl.append(listItemEl);
+//             break;
+//         case "lets collaborate":
+//             taskActionsEl.querySelector("select[name='status-change']").selectedIndex = 1;
+//             tasksInProgressEl.append(listItemEl);
+//             break;
+//         case "old ideas":
+//             taskActionsEl.querySelector("select[name='status-change']").selectedIndex = 2;
+//             tasksCompletedEl.append(listItemEl);
+//             break;
+//         default:
+//             console.log("Something went wrong!");
+//     }
 
-    tasks.push(taskDataObj);
+//     // save task as an object with name, type, status, and id properties then push it into tasks array
+//     taskDataObj.id = taskIdCounter;
 
-    // save tasks to localStorage
-    saveTasks();
+//     tasks.push(taskDataObj);
 
-    // increase task counter for next unique id
-    taskIdCounter++;
-};
+//     // save tasks to localStorage
+//     saveTasks();
 
-// create comment button
-const createTaskActions = function (taskId) {
-    // create container to hold elements
-    let actionContainerEl = document.createElement("div");
-    actionContainerEl.className = "task-actions";
+//     // increase task counter for next unique id
+//     taskIdCounter++;
+// };
 
-    // create edit button
-    let editButtonEl = document.createElement("button");
-    editButtonEl.textContent = "Edit";
-    editButtonEl.className = "btn edit-btn";
-    editButtonEl.setAttribute("data-task-id", taskId);
-    actionContainerEl.appendChild(editButtonEl);
-    // create delete button
-    let deleteButtonEl = document.createElement("button");
-    deleteButtonEl.textContent = "Delete";
-    deleteButtonEl.className = "btn delete-btn";
-    deleteButtonEl.setAttribute("data-task-id", taskId);
-    actionContainerEl.appendChild(deleteButtonEl);
-    // create change status dropdown
-    let statusSelectEl = document.createElement("select");
-    statusSelectEl.setAttribute("name", "status-change");
-    statusSelectEl.setAttribute("data-task-id", taskId);
-    statusSelectEl.className = "select-status";
-    actionContainerEl.appendChild(statusSelectEl);
-    // create status options
-    let statusChoices = ["New Ideas", "Lets Collaborate", "Old Ideas"];
+// // create comment button
+// const createTaskActions = function (taskId) {
+//     // create container to hold elements
+//     let actionContainerEl = document.createElement("div");
+//     actionContainerEl.className = "task-actions";
 
-    for (var i = 0; i < statusChoices.length; i++) {
-        // create option element
-        let statusOptionEl = document.createElement("option");
-        statusOptionEl.setAttribute("value", statusChoices[i]);
-        statusOptionEl.textContent = statusChoices[i];
+//     // create edit button
+//     let editButtonEl = document.createElement("button");
+//     editButtonEl.textContent = "Edit";
+//     editButtonEl.className = "btn edit-btn";
+//     editButtonEl.setAttribute("data-task-id", taskId);
+//     actionContainerEl.appendChild(editButtonEl);
+//     // create delete button
+//     let deleteButtonEl = document.createElement("button");
+//     deleteButtonEl.textContent = "Delete";
+//     deleteButtonEl.className = "btn delete-btn";
+//     deleteButtonEl.setAttribute("data-task-id", taskId);
+//     actionContainerEl.appendChild(deleteButtonEl);
+//     // create change status dropdown
+//     let statusSelectEl = document.createElement("select");
+//     statusSelectEl.setAttribute("name", "status-change");
+//     statusSelectEl.setAttribute("data-task-id", taskId);
+//     statusSelectEl.className = "select-status";
+//     actionContainerEl.appendChild(statusSelectEl);
+//     // create status options
+//     let statusChoices = ["New Ideas", "Lets Collaborate", "Old Ideas"];
 
-        // append to select
-        statusSelectEl.appendChild(statusOptionEl);
-    }
+//     for (var i = 0; i < statusChoices.length; i++) {
+//         // create option element
+//         let statusOptionEl = document.createElement("option");
+//         statusOptionEl.setAttribute("value", statusChoices[i]);
+//         statusOptionEl.textContent = statusChoices[i];
 
-    return actionContainerEl;
-};
+//         // append to select
+//         statusSelectEl.appendChild(statusOptionEl);
+//     }
+
+//     return actionContainerEl;
+// };
 
 
